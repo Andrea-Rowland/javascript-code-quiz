60eb898dcc35282453dda729d78cb6dbf486020b
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-08 09:50:18 -0600

    added timer

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
+const timeText = document.querySelector(".timer .time_left_text");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
+
 
 // If Start Quiz Button Clicked
 start_btn.onclick = ()=>{
@@ -19,10 +23,14 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); // hide the info box
     quiz_box.classList.add("activeQuiz"); // show the quiz box
-    showQuestions(0);
+    showQuestions(0); // calling showQuestions function
+    queCounter(1); // passing 1 parameter to queCounter
+    startTimer(10); // calling startTimer function
 }
 
 let que_count = 0;
+let que_numb = 1;
+let counter;
 
 const next_btn = document.querySelector(".next_btn");
 
@@ -30,7 +38,9 @@ const next_btn = document.querySelector(".next_btn");
 next_btn.onclick = ()=> {
     if(que_count < questions.length - 1){
         que_count++;
+        que_numb++;
         showQuestions(que_count);
+        queCounter(que_numb);
     }else{
         console.log("Questions completed");
     }
@@ -40,7 +50,6 @@ next_btn.onclick = ()=> {
 // getting questions and options from array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question + '</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                      + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
@@ -48,7 +57,54 @@ function showQuestions(index){
                      + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag; 
     option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for(let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
 }
 
-const bottom_ques_counter = quiz_box.querySelector(".total_que");
-let totalQuesCountTag = <span><p>2</p>of<p>5</p> Questions</span>
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>'
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>'
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length
+    if(userAns == correctAns) {
+        answer.classList.add("correct");
+    console.log("Answer is Correct");
+    answer.insertAdjacentHTML ("beforeend", tickIcon);
+} else {
+    answer.classList.add("incorrect");
+    console.log("Answer is Wrong");
+    answer.insertAdjacentHTML ("beforeend", crossIcon);
+
+    // if answer is incorrect then automatically select the correct answer
+    for (let i = 0; i < allOptions; i++) {
+        if(option_list.children[i].textContent == correctAns) {
+            option_list.children[i].setAttribute("class", "option correct");
+            option_list.children[i].insertAdjacentHTML ("beforeend", tickIcon);
+        }
+    }
+}
+
+//once user seleted disables all options
+for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+}
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; // decrement the time value
+    }
+}
+
+function queCounter(index) {
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
+
